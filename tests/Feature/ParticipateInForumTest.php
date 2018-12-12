737d9bfbb273a37764591fe0f477a4b8a377e36b
corean <corean@corean.biz>
2018-12-13 00:10:15 +0900

    episode 31. Authorized Users Can Delete Replies

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -14,44 +14,66 @@ class ParticipateInForumTest extends TestCase
     use DatabaseMigrations;
     protected $thread;
     
-    protected function setUp()
-    {
-        parent::setUp();
-        $this->thread = create(Thread::class);
-    }
-    
-    
     /** @test */
     function 미인증된_사용자는_댓글_등록_실패()
     {
+        $thread = create(Thread::class);
         // 인증된 사용자
         //$this->expectException(\Illuminate\Auth\AuthenticationException::class);
         // 포럼글에 댓글 남기기
         $this->withExceptionHandling()
-            ->post($this->thread->path() . '/replies', [])
-            ->assertRedirect('/login');
+             ->post($thread->path() . '/replies', [])
+             ->assertRedirect('/login');
     }
     
     /** @test */
-    function 인증된_사용자_포럼내_참여하는_방법()
+    function 인증된_사용자_포럼내_댓글_등록()
     {
         // 인증된 사용자
         $this->be($user = factory(User::class)->create());
         $reply = make(Reply::class);
-        
+        $thread = create(Thread::class);
         // 포럼글에 댓글 남기기
-        $this->post($this->thread->path() . '/replies', $reply->toArray());
+        $this->post($thread->path() . '/replies', $reply->toArray());
         // 댓글은 포럼글에 보여주기
-        $this->get($this->thread->path())->assertSee($reply->body);
+        $this->get($thread->path())
+             ->assertSee($reply->body);
     }
     
     /** @test */
-    function 댓글_내용이_있는지() {
-        $this->withExceptionHandling()->signIn();
+    function 댓글_내용이_있는지()
+    {
+        $this->withExceptionHandling()
+             ->signIn();
+        $thread = create(Thread::class);
         
-        $reply = make('App\Reply', ['body'=>null]);
-        $this->post($this->thread->path() . '/replies', $reply->toArray())
-        ->assertSessionHasErrors('body');
+        $reply = make('App\Reply', ['body' => null]);
+        $this->post($thread->path() . '/replies', $reply->toArray())
+             ->assertSessionHasErrors('body');
+    }
+    
+    /** @test */
+    function 미인증_사용자는_댓글을_삭제할수_없음()
+    {
+        $this->withExceptionHandling();
+        $reply = create('App\Reply');
+        
+        $this->delete('/replies/' . $reply->id)
+             ->assertRedirect('/login');
+        
+        $this->signIn()
+             ->delete('/replies/' . $reply->id)
+             ->assertStatus(403);
+    }
+    
+    /** @test */
+    function 인증된_사용자는_댓글을_삭제할수_있음()
+    {
+        $user = $this->signIn();
+        $reply = create('App\Reply', [ 'user_id' => auth()->id()]);
+        
+        $this->delete('/replies/' . $reply->id)->assertStatus(302);
+        $this->assertDatabaseMissing('replies', ['body' => $reply->body]);
         
     }
 }
